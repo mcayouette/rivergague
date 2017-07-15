9791d60de425b14c4fff2a6d8dac0797eb20bfdf
Martin <mcayouette@gmail.com>
2017-07-15 00:08:40 -0400

    Remove python request module

--- river.py
+++ river.py
@@ -1,12 +1,10 @@
-import requests, urllib
+import urllib
 import logging
 import csv
 
 WATEROFFICE="http://dd.weather.gc.ca/hydrometric/csv/ON/hourly/"
 BRITANNIA="ON_02KF005"
 
-# https://wateroffice.ec.gc.ca/report/real_time_e.html?stn=02KF005&mode=Table
-# http://dd.weather.gc.ca/hydrometric/csv/ON/hourly/ON_02KF005_hourly_hydrometric.csv
 def getStationCSV(stationName):
     if stationName == BRITANNIA:
         logger.debug("Found Station:" + stationName)
@@ -21,8 +19,8 @@ def getCurrentWaterLevel(stationName):
     # Download the CSV
     file = urllib.URLopener()
     file.retrieve(url, ''.join("tmp.csv"))
-    download = requests.get(url)
     reader = csv.reader(open("tmp.csv"))
+    # Get the last entry of the CSV to get the latest water level
     waterLevel = ""
     for row in reader:
         logging.debug("CSV " + ''.join(row))
